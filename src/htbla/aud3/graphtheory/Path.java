4d9cd5460ea11e47aa7d3e9816e1554d5382a0b7
David Lienbacher <lienbacherd@outlook.com>
2020-11-13 10:37:31 +0100

    computeDistance

--- src/htbla/aud3/graphtheory/Path.java
+++ src/htbla/aud3/graphtheory/Path.java
@@ -1,16 +1,25 @@
 package htbla.aud3.graphtheory;
 
+import java.util.List;
+
 /**
  * @author Jan Fuehrer, David Lienbacher, Fabian Psutka
  */
 public class Path {
+
+    private List<Edge> edges;
+
     
     public int[] getNodeIds() {
         return null;
     }
     
     public double computeDistance() {
-        return -1.0;
+        double distance = 0.0;
+        for (Edge e : edges) {
+            distance = distance + e.getEdgeWeight();
+        }
+        return distance;
     }
     
 }
