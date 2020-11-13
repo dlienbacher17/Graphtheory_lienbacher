af0f55f97f3dc4692487ea053e182066ac40b91a
DawnOfDiscovery <fabianpsutka@gmx.net>
2020-11-13 10:44:10 +0100

    Update Path.java

--- src/htbla/aud3/graphtheory/Path.java
+++ src/htbla/aud3/graphtheory/Path.java
@@ -1,12 +1,25 @@
 package htbla.aud3.graphtheory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Jan Fuehrer, David Lienbacher, Fabian Psutka
  */
 public class Path {
     
+    private List<Edge> edges;
+    
+    public Path(List<Edge> edges) {
+        this.edges = edges;
+    }
+    
     public int[] getNodeIds() {
-        return null;
+        int[] ids = edges.stream().mapToInt(e -> e.getFromNodeId()).toArray();
+        int lastIndex = edges.size() - 1;
+        ids[lastIndex] = edges.get(lastIndex).getToNodeId();
+        return ids;
     }
     
     public double computeDistance() {
