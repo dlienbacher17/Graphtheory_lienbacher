6c9d08d00307cf1c59c0efa867ec8be4ac83389d
Jan Führer <jan_fue@icloud.com>
2021-01-13 20:34:09 +0100

    added invalid test for flow

--- src/htbla/aud3/graphtheory/tests/GraphTest.java
+++ src/htbla/aud3/graphtheory/tests/GraphTest.java
@@ -147,6 +147,22 @@ public class GraphTest {
     }
 
     @Test
+    public void testDetermineMaximumFlowInvalid_int_int() {
+        System.out.println("determineMaximumFlow()");
+
+        Graph graph = new Graph();
+        graph.read(new File(linz_flussproblem_path));
+
+        int fromNodeId = -1;
+        int toNodeId = 1;
+
+        int expectedDistance = 0;
+        int returnedDistance = (int) graph.determineMaximumFlow(fromNodeId, toNodeId);
+
+        assertEquals(expectedDistance, returnedDistance, 0.0000000001);
+    }
+
+    @Test
     public void testDetermineBottleneck_int_int() {
         System.out.println("determineBottlenecks()");
 
